662e3230b73d41fdc44b37b083936420136a123a
Bethany Yunker <bayunker@owu.edu>
2012-02-05 11:44:59 -0500

    First commit for this file

---
+++ hello.py
@@ -0,0 +1,11 @@
+import sys
+# This takes in a name, and prints it out
+
+# This line takes the input
+x = raw_input("Enter your name: ")
+
+# This line prints it out
+print 'Hello,', x
+
+#sys.exec('say \'hello\'')
+
